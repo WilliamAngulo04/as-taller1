28069728dc496e4e0ace97c03d6b97c7477cc073
WilliamAngulo04 <williamquerry2@gmail.com>
2026-02-10 20:02:19 -0500

    Terminé hasta http

--- 04_http/servidor.py
+++ 04_http/servidor.py
@@ -1,2 +1,13 @@
 # Servidor
+import socket
+import http.server
+
+HOST = 'localhost'
+PORT = 9000
+
+class Servidor(http.server.SimpleHTTPRequestHandler):
+    pass
+
+servidor = http.server.HTTPServer((HOST, PORT), Servidor)
+servidor.serve_forever()
 
